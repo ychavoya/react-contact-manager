8e0ab05647d8ff400c54e471872f6b54f242af65
Yael Chavoya <realidadvirtualwm@gmail.com>
2018-10-10 13:46:14 -0500

    Updates

---
+++ precache-manifest.40d3626fc2a23485969d2d27644a56bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac3c48b9cdbff86df9e4",
+    "url": "/react-contact-manager/static/js/runtime~main.ac3c48b9.js"
+  },
+  {
+    "revision": "e57b52139761eb4132c7",
+    "url": "/react-contact-manager/static/js/main.e57b5213.chunk.js"
+  },
+  {
+    "revision": "166b361339eefb6651dc",
+    "url": "/react-contact-manager/static/js/1.166b3613.chunk.js"
+  },
+  {
+    "revision": "e57b52139761eb4132c7",
+    "url": "/react-contact-manager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "166b361339eefb6651dc",
+    "url": "/react-contact-manager/static/css/1.2a3c478b.chunk.css"
+  },
+  {
+    "revision": "934c66d784a803bac1702ccdf20100c2",
+    "url": "/react-contact-manager/index.html"
+  }
+];
